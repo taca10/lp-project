774345ef6573e24b4b072fbd53d959c4d3728b73
taca10 <taka.ur.0505@gmail.com>
2018-10-25 22:38:15 +0900

    deploy

---
+++ firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyByUVTP7SigFA8Mk4k4lHytmb-PxgJ705o",
+  authDomain: "lp-project-1e2cd.firebaseapp.com",
+  databaseURL: "https://lp-project-1e2cd.firebaseio.com",
+  projectId: "lp-project-1e2cd",
+  storageBucket: "lp-project-1e2cd.appspot.com",
+  messagingSenderId: "642883855856"
+};
